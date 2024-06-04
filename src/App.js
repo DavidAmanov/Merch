6da208b4c510021221b6691752d4dd407713c06d
David Ladaev <174save@gmail.com>
2024-06-04 17:08:29 +0300

    create some components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Catalog from './components/Catalog/Catalog.tsx';
+import Footer from './components/Footer/Footer.tsx';
+import Header from './components/Header/Header.tsx';
 
 function App() {
   return (
-    <div className="App">
+    <div className='app'>
+      <Header />
+      <Catalog />
+      <Footer />
     </div>
   );
 }
