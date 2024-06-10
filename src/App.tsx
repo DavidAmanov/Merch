c55833636a6f3c6768b2002f6ab4a74998572092
David Ladaev <174save@gmail.com>
2024-06-10 22:21:53 +0300

    add ts to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
-import Catalog from './components/Catalog/Catalog.tsx';
-import Footer from './components/Footer/Footer.tsx';
-import Header from './components/Header/Header.tsx';
+import Catalog from './components/Catalog/Catalog';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
